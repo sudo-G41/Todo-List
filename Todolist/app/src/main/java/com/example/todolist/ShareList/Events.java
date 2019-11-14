9274acc156801be40a16cfcbc821aec1f24efdc2
sudo-g41 <usa7372@gmail.com>
2019-11-15 08:33:00 +0900

    시작

---
+++ Todolist/app/src/main/java/com/example/todolist/ShareList/Events.java
@@ -0,0 +1,33 @@
+package com.example.todolist.ShareList;
+
+public class Events {
+    String EVENT, TIME, DATE, MONTH, YEAR;
+
+    public Events(String ECENT, String TIME, String DATE, String MONTH, String YEAR) {
+        this.EVENT = ECENT;
+        this.TIME = TIME;
+        this.DATE = DATE;
+        this.MONTH = MONTH;
+        this.YEAR = YEAR;
+    }
+
+    public String getEVENT() {
+        return EVENT;
+    }
+
+    public String getTIME() {
+        return TIME;
+    }
+
+    public String getDATE() {
+        return DATE;
+    }
+
+    public String getMONTH() {
+        return MONTH;
+    }
+
+    public String getYEAR() {
+        return YEAR;
+    }
+}
---
+++ Todolist/app/src/main/java/com/example/todolist/ShareList/ShareCalendar_Activity.java
@@ -0,0 +1,41 @@
+package com.example.todolist.ShareList;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class ShareCalendar_Activity extends LinearLayout {
+    ImageButton NextBtn, PreviousBtn;
+    TextView currentDate;
+    GridView gridView;
+    private  static final int MAX_CALENDAR_DAYS = 42;
+    Calendar calendar = Calendar.getInstance(Locale.KOREAN);
+    Context context;
+
+    List<Date> data = new ArrayList<>();
+    List<Events> eventsList = new ArrayList<>();
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM yyyy", Locale.KOREAN);
+    SimpleDateFormat monthFomat = new SimpleDateFormat("MMMM",Locale.KOREAN);
+    SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy", Locale.KOREAN);
+
+    public ShareCalendar_Activity(Context context) {
+        super(context);
+    }
+
+    public ShareCalendar_Activity(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+    }
+}
